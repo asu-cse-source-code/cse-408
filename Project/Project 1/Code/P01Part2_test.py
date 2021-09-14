8428a26458adc375e846cd6a22d1b3a02fbf1f27
abspen1 <abspencer2097@gmail.com>
2021-09-13 17:40:16 -0700

    add visualization to file

--- Project/Project 1/Code/P01Part2_test.py
+++ Project/Project 1/Code/P01Part2_test.py
@@ -8,10 +8,18 @@ def implementation_test():
 
     files = os.listdir(posFolder)
 
+    highest_file = ""
+    lowest_file = ""
+    highest_score = 0
+    lowest_score = 0
+
     for file in files:
         sent_score = sentimentAnalysis.sentimentAnalysis(f'{posFolder}/{file}')
         print(file)
         print(f'Groundtruth: Positive, sentiment score: {sent_score}')
+        if sent_score > highest_score:
+            highest_score = sent_score
+            highest_file = file
 
     files = os.listdir(negFolder)
 
@@ -19,7 +27,12 @@ def implementation_test():
         sent_score = sentimentAnalysis.sentimentAnalysis(f'{negFolder}/{file}')
         print(file)
         print(f'Groundtruth: Negative, sentiment score: {sent_score}')
+        if sent_score < lowest_score:
+            lowest_score = sent_score
+            lowest_file = file
 
+    print(f'Highest file: {highest_file}')
+    print(f'Lowest file: {lowest_file}')
 
 
 if __name__ == "__main__":
